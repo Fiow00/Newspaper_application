a26156f99204b30b1fba865ea70fcf3109b9708e
Fiow00 <ahmedyhya5325500@gmail.com>
2025-06-06 08:00:50 +0300

    Added forms for blog app

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.http import HttpResponseRedirect
+
 from django.views.generic import ListView, DetailView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 
 from .models import Post
 
@@ -11,3 +15,18 @@ class BlogListView(ListView):
 class BlogDetailView(DetailView):
     model = Post
     template_name = "blog/post.html"
+
+class BlogCreateView(CreateView):
+    model = Post
+    template_name = "blog/post_new.html"
+    fields = ["title", "author", "body"]
+
+class BlogUpdateView(UpdateView):
+    model = Post
+    template_name = "blog/post_edit.html"
+    fields = ["title", "body"]
+
+class BlogDeleteView(DeleteView):
+    model = Post
+    template_name = "blog/post_delete.html"
+    success_url = reverse_lazy("blog:post_list")
