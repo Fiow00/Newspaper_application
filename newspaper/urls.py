7fad77e00b6ce62f5c4d20ed4b6e26a715146a83
fiow00 <ahmedyhya5325500@gmail.com>
2025-10-05 21:14:28 +0300

    added comment update and delete

--- newspaper/urls.py
+++ newspaper/urls.py
@@ -5,9 +5,16 @@ from . import views
 app_name = "newspaper"
 
 urlpatterns = [
+
+    # Article urls
     path("", views.ArticleListView.as_view(), name="article_list"),
     path("<int:pk>/", views.ArticleDetailView.as_view(), name="article_detail"),
     path("<int:pk>/edit/", views.ArticleUpdateView.as_view(), name="article_edit"),
     path("<int:pk>/delete/", views.ArticleDeleteView.as_view(), name="article_delete"),
     path("new/", views.ArticleCreateView.as_view(), name="article_new"),
+
+    # Comment urls
+    path('<int:pk>/comment/', views.CommentCreateView.as_view(), name='comment_new'),
+    path("<int:pk>/comment/edit/", views.CommentUpdateView.as_view(), name="comment_edit"),
+    path("<int:pk>/comment/delete/", views.CommentDeleteView.as_view(), name="comment_delete"),
 ]
